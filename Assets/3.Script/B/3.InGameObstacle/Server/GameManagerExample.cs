b1cefef3f29614e29c011acfac7f4702c7f3599a
RiverPark99 <phs201007@gmail.com>
2026-04-16 12:12:20 +0900

    로컬 서버에서 생성 완료

--- Assets/3.Script/B/3.InGameObstacle/Server/GameManagerExample.cs
+++ Assets/3.Script/B/3.InGameObstacle/Server/GameManagerExample.cs
@@ -6,6 +6,15 @@ public class GameManagerExample : NetworkBehaviour
     [SerializeField] private ObstacleSpawner _spawner;
     [SerializeField] private MapSpawner _map_spawner;
 
+    public override void OnStartServer() //나중에 신규 게임 시작할때나 끝났을때 미리 생성하던지 해야?
+    {
+        base.OnStartServer();
+
+        // 서버가 기동되면 즉시 맵 생성을 시작함
+        Debug.Log("서버가 시작되었습니다. 맵 생성을 트리거합니다.");
+        StartNewGame();
+    }
+
     [Server]
     public void StartNewGame()
     {
