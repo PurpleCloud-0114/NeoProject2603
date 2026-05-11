983b45fb62850f2dcd907a8fbeb282d7b6e6b8c3
RiverPark99 <phs201007@gmail.com>
2026-05-11 10:03:00 +0900

    Audiomanager, OptionController 보강

--- Assets/3.Script/B/0.Title/OptionController.cs
+++ Assets/3.Script/B/0.Title/OptionController.cs
@@ -1,14 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
-public enum ControlPreset
-{
-    JoyRight = 0,
-    JoyLeft = 1,
-    GyroRight = 2,
-    GyroLeft = 3
-}
+public enum ControlPreset { JoyRight = 0, JoyLeft = 1, GyroRight = 2, GyroLeft = 3 }
 
 public class OptionController : MonoBehaviour
 {
@@ -30,79 +23,67 @@ public class OptionController : MonoBehaviour
     [SerializeField] private Color _selected_color = Color.white;
     [SerializeField] private Color _unselected_color = Color.gray;
 
-    [Header("Gyro Slider Settings")]
+    [Header("Sliders")]
     [SerializeField] private Slider _sensitivity_slider;
-    [SerializeField] private Color _slider_disabled_color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
-    [SerializeField] private Image _sensitivity_fill_img;
-    [SerializeField] private Image _sensitivity_handle_img;
-
-    [Header("Other Sliders")]
     [SerializeField] private Slider _bgm_slider;
     [SerializeField] private Slider _sfx_slider;
+
+    [Header("Slider Visuals")]
+    [SerializeField] private Image _sensitivity_fill_img;
+    [SerializeField] private Image _sensitivity_handle_img;
+    [SerializeField] private Color _disabled_color = new Color(0.5f, 0.5f, 0.5f, 0.5f);
 
     [Header("Navigation")]
     [SerializeField] private Button _back_button;
 
-    private Color _original_fill_color = Color.white;
-    private Color _original_handle_color = Color.white;
-
-    // 현재 선택된 프리셋을 Enum 타입으로 관리
-    private ControlPreset _currentPreset = ControlPreset.JoyRight;
+    private Color _original_fill_color;
+    private Color _original_handle_color;
+    private ControlPreset _currentPreset;
 
     private void Start()
     {
-        InitSliderCaching();
+        // 초기 컬러 백업 및 슬라이더 이미지 캐싱
+        if (_sensitivity_slider != null)
+        {
+            if (_sensitivity_fill_img == null) _sensitivity_fill_img = _sensitivity_slider.fillRect.GetComponent<Image>();
+            if (_sensitivity_handle_img == null) _sensitivity_handle_img = _sensitivity_slider.handleRect.GetComponent<Image>();
+            _original_fill_color = _sensitivity_fill_img.color;
+            _original_handle_color = _sensitivity_handle_img.color;
+        }
 
-        // 데이터 로드 (int를 Enum으로 형변환)
+        // 데이터 로드 및 슬라이더 초기화
         _currentPreset = (ControlPreset)PlayerPrefs.GetInt(_PRESET_KEY, 0);
-
         _sensitivity_slider.value = PlayerPrefs.GetFloat(_SENSITIVITY_KEY, 6f);
-        _bgm_slider.value = PlayerPrefs.GetFloat(_BGM_KEY, 0.75f);
-        _sfx_slider.value = PlayerPrefs.GetFloat(_SFX_KEY, 0.75f);
+        _bgm_slider.value = PlayerPrefs.GetFloat(_BGM_KEY, 0.6f);
+        _sfx_slider.value = PlayerPrefs.GetFloat(_SFX_KEY, 0.6f);
 
-        // 리스너 등록 (Enum 값을 인자로 전달)
+        // 버튼 리스너 등록
         _joy_right_button.onClick.AddListener(() => SetPreset(ControlPreset.JoyRight));
         _joy_left_button.onClick.AddListener(() => SetPreset(ControlPreset.JoyLeft));
         _gyro_right_button.onClick.AddListener(() => SetPreset(ControlPreset.GyroRight));
         _gyro_left_button.onClick.AddListener(() => SetPreset(ControlPreset.GyroLeft));
         _back_button.onClick.AddListener(BackToTitle);
 
-        _sensitivity_slider.onValueChanged.AddListener((val) => PlayerPrefs.SetFloat(_SENSITIVITY_KEY, val));
-
+        // 슬라이더 리스너 (실시간 볼륨 조절)
         _bgm_slider.onValueChanged.AddListener((val) => {
             PlayerPrefs.SetFloat(_BGM_KEY, val);
-            if (AudioManager.Instance != null) AudioManager.Instance.SetVolume(_BGM_KEY, val);
+            if (AudioManager.Instance != null) AudioManager.Instance.SetVolume("BGM", val);
         });
 
         _sfx_slider.onValueChanged.AddListener((val) => {
             PlayerPrefs.SetFloat(_SFX_KEY, val);
-            if (AudioManager.Instance != null) AudioManager.Instance.SetVolume(_SFX_KEY, val);
+            if (AudioManager.Instance != null) AudioManager.Instance.SetVolume("SFX", val);
         });
 
+        _sensitivity_slider.onValueChanged.AddListener((val) => PlayerPrefs.SetFloat(_SENSITIVITY_KEY, val));
+
         UpdateControlUI();
-    }
-
-    private void InitSliderCaching()
-    {
-        if (_sensitivity_slider != null)
-        {
-            if (_sensitivity_fill_img == null && _sensitivity_slider.fillRect != null)
-                _sensitivity_fill_img = _sensitivity_slider.fillRect.GetComponent<Image>();
-
-            if (_sensitivity_handle_img == null && _sensitivity_slider.handleRect != null)
-                _sensitivity_handle_img = _sensitivity_slider.handleRect.GetComponent<Image>();
-        }
-
-        if (_sensitivity_fill_img != null) _original_fill_color = _sensitivity_fill_img.color;
-        if (_sensitivity_handle_img != null) _original_handle_color = _sensitivity_handle_img.color;
     }
 
     public void SetPreset(ControlPreset preset)
     {
         _currentPreset = preset;
-        // 저장할 때는 다시 int로 형변환
         PlayerPrefs.SetInt(_PRESET_KEY, (int)preset);
-        PlayerPrefs.Save();
         UpdateControlUI();
     }
 
@@ -113,26 +94,15 @@ public class OptionController : MonoBehaviour
         UpdateButtonStyle(_gyro_right_button, _currentPreset == ControlPreset.GyroRight);
         UpdateButtonStyle(_gyro_left_button, _currentPreset == ControlPreset.GyroLeft);
 
-        // 자이로 모드 판별 (Enum 기반)
-        bool isGyroMode = (_currentPreset == ControlPreset.GyroRight || _currentPreset == ControlPreset.GyroLeft);
-
-        if (_sensitivity_slider != null)
-        {
-            _sensitivity_slider.interactable = isGyroMode;
-            if (_sensitivity_fill_img != null)
-                _sensitivity_fill_img.color = isGyroMode ? _original_fill_color : _slider_disabled_color;
-            if (_sensitivity_handle_img != null)
-                _sensitivity_handle_img.color = isGyroMode ? _original_handle_color : _slider_disabled_color;
-        }
+        bool isGyro = (_currentPreset == ControlPreset.GyroRight || _currentPreset == ControlPreset.GyroLeft);
+        _sensitivity_slider.interactable = isGyro;
+        _sensitivity_fill_img.color = isGyro ? _original_fill_color : _disabled_color;
+        _sensitivity_handle_img.color = isGyro ? _original_handle_color : _disabled_color;
     }
 
     private void UpdateButtonStyle(Button btn, bool isSelected)
     {
-        if (btn == null) return;
-        if (btn.TryGetComponent(out Image img))
-        {
-            img.color = isSelected ? _selected_color : _unselected_color;
-        }
+        if (btn.TryGetComponent(out Image img)) img.color = isSelected ? _selected_color : _unselected_color;
         btn.interactable = !isSelected;
     }
 
